2aeb92b4f47b78e44212622f318fd8ebb6018bf9
louislagura <60904092+louislagura@users.noreply.github.com>
2020-02-12 20:27:20 +0800

    Added mysql database connection
    Added mysql database connection

--- inventory_crud/routes/index.js
+++ inventory_crud/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var mysql = require("mysql");
+var db = mysql.createPool({
+	host: "localhost",
+	user: "root",
+	password: "password",
+	databse: "inventory"
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+	res.render("index", { title: "Express" });
+});
+
+router.get("/testconnect", function(req, res, next) {
+	if (db != null) {
+		res.send("Connected Successfully");
+	} else {
+		res.send("Connection Failed");
+	}
 });
 
 module.exports = router;
